e5a4600e9f12fd77f6b20cc5cd9dcc5c52705d92
ambagwa <ambagwaeugene09@gmail.com>
2025-03-16 02:57:06 +0300

    Combined with the last commit
    display_country function ADDED
    
    display_month

--- feb.py
+++ feb.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+
 print("We are learning git and github")
 
 print("Hello Python world")
+
+def display_month():
+    now = datetime.now()
+
+    current_month = now.strftime("%B")
+
+    print(f"It's {current_month}")
+
+display_month()
---
+++ s_africa.py
@@ -0,0 +1,4 @@
+def display_country(name):
+    print(f"Hello world. This is {name}!")
+
+display_country("South Africa")
